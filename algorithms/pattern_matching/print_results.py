01b796e3bc5f29f470c187ed79bd04362f7ad96f
axel <axel@kali.local>
2020-12-07 17:12:20 +0100

    Run output processing

--- algorithms/pattern_matching/print_results.py
+++ algorithms/pattern_matching/print_results.py
@@ -1,58 +1,18 @@
 import sys
 import os
 from pathlib import Path
+from tqdm import tqdm
 
 # 6WLJ
 
 
-def hex_to_RGB(hex):
-    ''' "#FFFFFF" -> [255,255,255] '''
-    # Pass 16 to the integer function for change of base
-    return [int(hex[i:i+2], 16) for i in range(1, 6, 2)]
-
-
-def RGB_to_hex(RGB):
-    ''' [255,255,255] -> "#FFFFFF" '''
-    # Components need to be integers for hex to make sense
-    RGB = [int(x) for x in RGB]
-    return "#"+"".join(["0{0:x}".format(v) if v < 16 else
-                        "{0:x}".format(v) for v in RGB])
-
-
-def color_dict(gradient):
-    ''' Takes in a list of RGB sub-lists and returns dictionary of
-      colors in RGB and hex form for use in a graphing function
-      defined later on '''
-    return {"hex": [RGB_to_hex(RGB) for RGB in gradient],
-            "r": [RGB[0] for RGB in gradient],
-            "g": [RGB[1] for RGB in gradient],
-            "b": [RGB[2] for RGB in gradient]}
-
-
-def linear_gradient(start_hex, finish_hex="#FFFFFF", n=10):
-    ''' returns a gradient list of (n) colors between
-      two hex colors. start_hex and finish_hex
-      should be the full six-digit color string,
-      inlcuding the number sign ("#FFFFFF") '''
-    # Starting and ending colors in RGB form
-    s = hex_to_RGB(start_hex)
-    f = hex_to_RGB(finish_hex)
-    # Initilize a list of the output colors with the starting color
-    RGB_list = [s]
-    # Calcuate a color at each evenly spaced value of t from 1 to n
-    for t in range(1, n):
-        # Interpolate RGB vector for color at the current value of t
-        cv = [int(s[j] + (float(t)/(n-1))*(f[j]-s[j])) for j in range(3)]
-        # Add it to our list of output colors
-        RGB_list.append(cv)
-
-    return color_dict(RGB_list)
+colors = ["#FF0000", "#00FF00", "#0000FF"]
 
 
 def draw_structure(target, bps, path, varna):
     '''
     target : dot bracket seq
-    bps : ??
+    bps : [[[1, 2], [5, 10]], ]
     path : outputpath
     vanra : path to varna
     '''
@@ -65,34 +25,125 @@ def draw_structure(target, bps, path, varna):
         varna, seq, target, path)
     cmd += " -bpStyle simple -algorithm line -resolution 4"
 
-    # colors = linear_gradient("#ffcccc", "#ff0000", len(bps)+1)["hex"][1:]
-    # auxbps = ["({},{}):color={}".format(bp[0]+1, bp[1]+1, c)
-    #           for bp, c in zip(bps, colors)]
-    # cmd += " -auxBPs \"{}\"".format(";".join(auxbps))
-    cmd += ' -highlightRegion "10-16:fill=#FF0000"'
-    print(cmd)
+    auxhighlight = []
+
+    for i in range(len(bps)):
+        for j in range(len(bps[i])):
+            auxhighlight.append(
+                f"{bps[i][j][0]}-{bps[i][j][1]}:fill=#FFFFFF,outline={colors[i%3]},radius={16+j*2}")
+
+    cmd += " -highlightRegion \"{}\"".format(";".join(auxhighlight))
+
+    # print(cmd)
     os.popen(cmd).close()
 
 
-def find_end_pattern(start, pattern, seq):
-    bp_nb = 0
-    pos = start
-    for car in pattern:
-        if(car == '*'):
-            while(seq[pos])
+def find_end_pattern(start, seq):
+    count = 0
+
+    for i in range(start, len(seq)):
+        if seq[i] == '(':
+            count += 1
+        elif seq[i] == ')':
+            count -= 1
+
+        if(count == 0):
+            return i
 
 
-dbn = open("../dot_brackets/6WLJ.dbn")
-lignes = dbn.readlines()
-structure = lignes[2]
-sequence = lignes[1]
-dbn.close()
+def parse_seq(seq):
+    def fix(s):
+        pstack = []
+        fixed = [c for c in s]
 
-pattern_file = open("./all_sub_1.txt")
-pattern = pattern_file.readlines()[44]
-pattern_file.close()
+        for i, c in enumerate(s):
+            if c == '(':
+                pstack.append(i)
+            elif c == ')':
+                if len(pstack) == 0:
+                    fixed[i] = '.'
+                else:
+                    pstack.pop()
 
-pattern_pos = 5
+        while len(pstack) > 0:
+            fixed[pstack.pop()] = '.'
 
-draw_structure(structure, [], './ouput_varna.png',
-               '/home/axel/Téléchargements/VARNAv3-93.jar')
+        return ''.join(fixed)
+    chains = seq.strip().split('&')
+    return [fix(c) for c in chains]
+
+
+def contains_motif(line):
+    return len(line.split('\t')) > 2
+
+
+def treat_motif(dbn_path, out_path, output_path):
+    output = open(out_path)
+    lignes = output.readlines()
+    output.close()
+
+    if(any([contains_motif(l) for l in lignes])):
+        dbn = open(dbn_path)
+        temp = dbn.readlines()
+        structure = temp[2].strip()
+        dbn.close()
+
+        motifs = parse_seq(structure)
+
+        assert(len(motifs) == len(lignes))
+
+        for i, ligne in enumerate(lignes):
+            if contains_motif(ligne):
+                name = ligne.strip().split('\t')[0]
+                motifs_occurence = ligne.strip().split('\t')[1:]
+                motifs_positions = []
+
+                for occurence in motifs_occurence:
+                    positions = occurence.strip('()').split(',')[1:]
+
+                    motifs_positions.append(
+                        [[int(pos), find_end_pattern(int(pos), motifs[i])] for pos in positions])
+
+                # print(motifs_positions)
+
+                draw_structure(
+                    motifs[i], motifs_positions, f'{output_path}/{name}.png', '/home/axel/Téléchargements/VARNAv3-93.jar')
+
+
+def main():
+    if len(sys.argv) < 4:
+        print(
+            f"Usage : python {sys.argv[0]} output_files dbn_files output_path")
+        return
+
+    output_pathlist = [str(path)
+                       for path in Path(sys.argv[1]).glob('**/*.out')]
+    dbn_pathlist = [str(path) for path in Path(sys.argv[2]).glob('**/*.dbn')]
+
+    for out_path in tqdm(output_pathlist):
+        name = out_path.split('/')[-1].split('.')[0]
+
+        dbn_paths = list(filter(lambda x: name in x, dbn_pathlist))
+        if len(dbn_paths) > 0:
+            dbn_path = dbn_paths[0]
+
+            treat_motif(dbn_path, out_path, sys.argv[3])
+        else:
+            continue
+
+
+if __name__ == '__main__':
+    main()
+
+    # because path is object not string
+
+    # print(path_in_str)
+
+# # pattern_file = open("all_sub_1.txt")
+# # pattern = pattern_file.readlines()[44]
+# # pattern_file.close()
+
+# pattern_pos = 5
+
+# draw_structure(structure, [[5, find_end_pattern(5, structure)], [10, 50]], './ouput_varna.png',
+#                '/home/axel/Téléchargements/VARNAv3-93.jar')
