008a0bb91bb773722eb76152f33ba69ddfb72d76
axel <axel@kali.local>
2020-12-15 10:39:51 +0100

    Run json 5

--- algorithms/pattern_matching/print_results.py
+++ algorithms/pattern_matching/print_results.py
@@ -114,7 +114,10 @@ def treat_xdbn(xdbn_path, out_path, motif_path, output_path):
         chaines = [parse_seq(xdbn_lignes[2*n])
                    for n in range(1, len(xdbn_lignes)//2+1)]
 
-        #print(chaines)
+        try:
+            isoCanonPairs= [[int(s.split('-')[0]), int(s.split('-')[1])] for s in xdbn_lignes[0].strip().split(';')[:-1]]
+        except:
+            return
 
         motifs_file = open(motif_path)
         motifs = motifs_file.readlines()
@@ -122,7 +125,7 @@ def treat_xdbn(xdbn_path, out_path, motif_path, output_path):
 
         assert(sum([len(chaine) for chaine in chaines]) == len(lignes_output))
 
-        for i, ligne in enumerate(lignes_output):
+        for ligne in lignes_output:
             if contains_motif(ligne):
                 name = ligne.strip().split('\t')[0]
                 chain_number = int(name.split('-')[1])
@@ -136,9 +139,17 @@ def treat_xdbn(xdbn_path, out_path, motif_path, output_path):
                     motif = motifs[int(occurence.strip().split(':')[0])]
 
                     positions = occurence.split(':')[-1].split(';')[:-1]
+                    interval = [find_intervals(motif, chaines[subchain_number][chain_number], int(pos.split('-')[0])) for pos in positions]
+                    print(interval)
+                #     if not any([(x[0] <= interval[1] and x[0] >= interval[0] and x[1] >= interval[0] and x[1] <= interval[1]) for x in isoCanonPairs]):
+                #         motifs_positions += interval
+                #     else:
+                #         print("Motif with non cano isolated base pair found in "+name)
+                
+                # draw_structure(
+                #     chaines[subchain_number][chain_number], motifs_positions, f'{output_path}/{name}.png', '/home/axel/Téléchargements/VARNAv3-93.jar')
 
-                    motifs_positions += [find_intervals(
-                        motif, chaines[i], int(pos.split('-')[0])) for pos in positions]
+                    
 
 
 def treat_motif(dbn_path, out_path, motif_path, output_path):
